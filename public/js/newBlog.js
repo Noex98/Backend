f72d61edfd59adbf30f1cb4a90939e1839013525
Noex98 <johannes.knickering@hotmail.com>
2021-04-14 14:36:34 +0200

    fixes

--- public/js/newBlog.js
+++ public/js/newBlog.js
@@ -3,6 +3,14 @@ const postForm = document.getElementById('postForm');
 const formBody = document.getElementById('form__body');
 const charLeft = document.getElementById('charLeft')
 
+setInterval(function(){ 
+    if (newPostBtn.innerHTML == 'New post|'){
+        newPostBtn.innerHTML = 'New post'
+    } else {
+        newPostBtn.innerHTML = 'New post|'
+    }
+}, 550);
+
 // Open new blog form
 newPostBtn.addEventListener('click', () => {
     newPostBtn.style.display ="none";
