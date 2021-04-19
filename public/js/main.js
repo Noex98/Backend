96dd74078590b6c334d8a12e026865ef69e2604f
Noex98 <johannes.knickering@hotmail.com>
2021-04-19 19:58:37 +0200

    .

--- public/js/main.js
+++ public/js/main.js
@@ -1,33 +1,41 @@
-(function navSlide() {
-	const burger = document.querySelector('.burger');
-	const nav = document.getElementsByTagName('aside')[0];
-	const navLinks = document.querySelectorAll('.nav__links li');
-	
-	burger.addEventListener('click', () => {
-		//Toggle Nav
-		nav.classList.toggle('nav-active');
+const burger = document.querySelector('.burger');
+const nav = document.getElementsByTagName('aside')[0];
+const navLinks = document.querySelectorAll('.nav__links li');
+const htmlOverlay = document.getElementById('htmlOverlay');
 
-        //prevent scroll while active nav
-        if (nav.classList.contains("nav-active")) {
-            document.body.style.overflow = "hidden"
-        } else {
-            document.body.style.overflow = "auto"
-            
-        }
-        
-        //Scroll to top
-        window.scrollTo(0,0);
+function burgerClick(){
+	//Toggle Nav
+	nav.classList.toggle('nav-active');
+
+	//prevent scroll while active nav
+	if (nav.classList.contains("nav-active")) {
+		document.body.style.overflow = "hidden"
+		htmlOverlay.style.opacity = 0.2;
+		htmlOverlay.style.pointerEvents = "auto"
 		
-		//animate Links
-		navLinks.forEach((link, index) => {
-			if (link.style.animation) {
-				link.style.animation = ''
-			} else {
-				link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-			}
-		});
+	} else {
+		document.body.style.overflow = "auto"
+		htmlOverlay.style.opacity = 0;
+	}
 
-		//Burger Animation
-		burger.classList.toggle('burger-toggle')
+	//Scroll to top
+	scrollTo(0,0);
+
+	//animate Links
+	navLinks.forEach((link, index) => {
+		if (link.style.animation) {
+			link.style.animation = ''
+		} else {
+			link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.2}s`;
+		}
 	});
-})();
+
+	//Burger Animation
+	burger.classList.toggle('burger-toggle')
+}
+
+//Clicking dark area closes the nav
+htmlOverlay.addEventListener('click', () => { burgerClick() })
+
+// Burger menu clicked
+burger.addEventListener('click', () => { burgerClick() });
